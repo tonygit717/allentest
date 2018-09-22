0622fd1b7c532e713d038a568423927837f596d7
allenchen <641390846@qq.com>
2018-09-22 17:32:58 +0800

    About Json

---
+++ TestJsonObject.java
@@ -0,0 +1,14 @@
+package Demo;
+
+
+//import org.json.*;
+
+public class TestJsonObject {
+
+	public static void main(String[] args) throws Exception {
+	String str = "{\"id_no\":\"342422198012001122\",\"mobile_no\":\"13666667789\",\"name\":\"徐丽\"}";
+	//JSONObject obj = new JSONObject(str);  //在这里转换。
+	//System.out.println(obj);
+	//System.out.println(obj.get("id_no")); // "342422198012001122"}  }
+	}
+}
